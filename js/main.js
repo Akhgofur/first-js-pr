410ae65390b4b273850b8fb6f92cb82c7e752f8f
Akhgofur <gofurbekaxmadjonov397@gmail.com>
2022-05-15 12:16:06 +0500

    js project

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let num = prompt('sonni kiriting')
+if(num%2==0){
+    alert('bu son juft son')
+}else{
+    alert('bu son toq son')
+}
---
+++ js/voenkomat.js
@@ -0,0 +1,10 @@
+let firstName = prompt('isminigiz')
+let lastName = prompt('familiyangiz')
+let birthYear = prompt('tug\'ilgan yilingiz')
+let year = 2022-birthYear
+if(year<18){
+    alert(firstName + '\n' + lastName + '\n' + 'yoshingiz : ' + year + '\n' + 'siz hali balog\'at yoshiga yetmagansiz')
+}
+else if(year>=18){
+    alert(firstName + '\n' + lastName + '\n' + 'yoshingiz : ' + year + '\n' + 'siz armiyaga qabul qilinasiz')
+}
